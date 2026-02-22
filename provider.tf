113640249761d43656790f2a765c398ab8656d65
Leon Maister (pr) <LM28@hotmail.com>
2026-02-22 18:43:36 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,23 @@
 terraform {
   required_providers {
+    # Основной провайдер для работы с Akeyless
     vault = {
       source  = "hashicorp/vault"
       version = "3.25.0"
     }
+    # Оставляем этот блок, чтобы Terraform не падал при поиске старых ресурсов
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
+# Настройка подключения к Akeyless
 provider "vault" {
   address = "https://hvp.akeyless.io"
+  # Токен подставит env0 автоматически через переменную VAULT_TOKEN
 }
+
+# Пустой блок для корректного удаления старых ресурсов
+provider "random" {}
