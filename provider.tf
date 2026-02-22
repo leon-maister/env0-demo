be8e2789b4b8517a7863cb6fdc8607ae6226ba72
Leon Maister (pr) <LM28@hotmail.com>
2026-02-22 17:48:44 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_version = "~> 1.6.0"
-
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "3.25.0"
-    }
+required_providers {
+  vault = {
+    source = "hashicorp/vault"
   }
-}
-provider "vault" {
-  address = "https://hvp.akeyless.io"
 }
