a6a1d6d77b781f9585fe15ae57aea5c03ad0ff47
Leon Maister (pr) <LM28@hotmail.com>
2026-02-22 17:10:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
-output "hello" {
-  value = "Hello from env0! Infrastructure as Code is ready."
+# This data source tells Terraform to fetch a specific secret from Akeyless
+data "vault_generic_secret" "my_first_secret" {
+  path = "MyFirstSecret" # The exact name/path of your secret in Akeyless
 }
+
+# This output will show the length of the secret to confirm we got it
+# (without printing the actual sensitive value in plain text)
+output "secret_check" {
+  value     = "Successfully retrieved secret! Length: ${length(data.vault_generic_secret.my_first_secret.data["value"])}"
+}
+
+# If you really want to see the value (for testing only!), uncomment the lines below:
+# output "actual_secret_value" {
+#   value     = data.vault_generic_secret.my_first_secret.data["value"]
+#   sensitive = true
+# }
