0156a67f6e82e4862165fe24cec7e72b45c9f434
Leon Maister (pr) <LM28@hotmail.com>
2026-02-22 18:10:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
-# This data source tells Terraform to fetch a specific secret from Akeyless
-data "vault_generic_secret" "my_first_secret" {
-  path = "MyFirstSecret" # The exact name/path of your secret in Akeyless
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
 }
 
-# This output will show the length of the secret to confirm we got it
-# (without printing the actual sensitive value in plain text)
-output "secret_check" {
-  value     = "Successfully retrieved secret! Length: ${length(data.vault_generic_secret.my_first_secret.data["value"])}"
-}
+provider "random" {}
 
-# If you really want to see the value (for testing only!), uncomment the lines below:
-# output "actual_secret_value" {
-#   value     = data.vault_generic_secret.my_first_secret.data["value"]
-#   sensitive = true
-# }
+resource "random_pet" "test" {
+  length = 2
+}
